afdd5a54d2d02be5f8c1c485d8a579de29f68b00
William Melody <hi@williammelody.com>
2016-01-14 18:13:28 -0800

    Use indented 'rails'-style access modifier indentation.
    An extra indentation level for non-public methods immediately identifies
    them as being acted upon by an access modifer even when the modifier
    keyword is not displayed on the screen.
    
    rubocop: Style/AccessModifierIndentation
    rubocop: Style/IndentationConsistency

--- lib/pandoc-ruby.rb
+++ lib/pandoc-ruby.rb
@@ -170,108 +170,108 @@ class PandocRuby
     end
   end
 
-private
+  private
 
-  # Executes the pandoc command for binary writers. A temp file is created
-  # and written to, then read back into the program as a string, then the
-  # temp file is closed and unlinked.
-  def convert_binary
-    tmp_file = Tempfile.new('pandoc-conversion')
-    begin
-      self.options += [{:output => tmp_file.path}]
-      self.option_string = "#{self.option_string} --output #{tmp_file.path}"
-      execute(command_with_options)
-      return IO.binread(tmp_file)
-    ensure
-      tmp_file.close
-      tmp_file.unlink
-    end
-  end
-
-  # Executes the pandoc command for btring writers.
-  def convert_string
-    execute(command_with_options)
-  end
-
-  # Combines the executable string with the option string.
-  def command_with_options
-    @@pandoc_path + self.option_string
-  end
-
-  # Runs the command and returns the output.
-  def execute(command)
-    output = error = exit_status = nil
-    @timeout ||= 31_557_600 # A year should be enough?
-    Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+    # Executes the pandoc command for binary writers. A temp file is created
+    # and written to, then read back into the program as a string, then the
+    # temp file is closed and unlinked.
+    def convert_binary
+      tmp_file = Tempfile.new('pandoc-conversion')
       begin
-        Timeout.timeout(@timeout) do
-          stdin.puts @target
-          stdin.close
-          output = stdout.read
-          error = stderr.read
-          exit_status = wait_thr.value
-        end
-      rescue Timeout::Error => ex
-        Process.kill 9, wait_thr.pid
-        maybe_ex = "\n#{ex}" if ex
-        error = "Pandoc timed out after #{@timeout} seconds.#{maybe_ex}"
+        self.options += [{:output => tmp_file.path}]
+        self.option_string = "#{self.option_string} --output #{tmp_file.path}"
+        execute(command_with_options)
+        return IO.binread(tmp_file)
+      ensure
+        tmp_file.close
+        tmp_file.unlink
       end
     end
 
-    fail error unless exit_status && exit_status.success?
-    output
-  end
-
-  # Builds the option string to be passed to pandoc by iterating over the
-  # opts passed in. Recursively calls itself in order to handle hash options.
-  def prepare_options(opts = [])
-    opts.inject('') do |string, (option, value)|
-      string += case
-                when value
-                  create_option(option, value)
-                when option.respond_to?(:each_pair)
-                  prepare_options(option)
-                else
-                  create_option(option)
-                end
+    # Executes the pandoc command for btring writers.
+    def convert_string
+      execute(command_with_options)
     end
-  end
 
-  # Takes a flag and optional argument, uses it to set any relevant options
-  # used by the library, and returns string with the option formatted as a
-  # command line options. If the option has an argument, it is also included.
-  def create_option(flag, argument = nil)
-    return '' unless flag
-    flag = flag.to_s
-    set_pandoc_ruby_options(flag, argument)
-    return '' if flag == 'timeout' # pandoc doesn't accept timeouts yet
-    if !argument.nil?
-      "#{format_flag(flag)} #{argument}"
-    else
-      format_flag(flag)
+    # Combines the executable string with the option string.
+    def command_with_options
+      @@pandoc_path + self.option_string
     end
-  end
 
-  # Formats an option flag in order to be used with the pandoc command line
-  # tool.
-  def format_flag(flag)
-    if flag.length == 1
-      " -#{flag}"
-    else
-      " --#{flag.to_s.tr('_', '-')}"
-    end
-  end
+    # Runs the command and returns the output.
+    def execute(command)
+      output = error = exit_status = nil
+      @timeout ||= 31_557_600 # A year should be enough?
+      Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+        begin
+          Timeout.timeout(@timeout) do
+            stdin.puts @target
+            stdin.close
+            output = stdout.read
+            error = stderr.read
+            exit_status = wait_thr.value
+          end
+        rescue Timeout::Error => ex
+          Process.kill 9, wait_thr.pid
+          maybe_ex = "\n#{ex}" if ex
+          error = "Pandoc timed out after #{@timeout} seconds.#{maybe_ex}"
+        end
+      end
 
-  # Takes an option and optional argument and uses them to set any flags
-  # used by PandocRuby.
-  def set_pandoc_ruby_options(flag, argument = nil)
-    case flag
-    when 't', 'to'
-      self.writer = argument.to_s
-      self.binary_output = true if BINARY_WRITERS.keys.include?(self.writer)
-    when 'timeout'
-      @timeout = argument
+      fail error unless exit_status && exit_status.success?
+      output
     end
-  end
+
+    # Builds the option string to be passed to pandoc by iterating over the
+    # opts passed in. Recursively calls itself in order to handle hash options.
+    def prepare_options(opts = [])
+      opts.inject('') do |string, (option, value)|
+        string += case
+                  when value
+                    create_option(option, value)
+                  when option.respond_to?(:each_pair)
+                    prepare_options(option)
+                  else
+                    create_option(option)
+                  end
+      end
+    end
+
+    # Takes a flag and optional argument, uses it to set any relevant options
+    # used by the library, and returns string with the option formatted as a
+    # command line options. If the option has an argument, it is also included.
+    def create_option(flag, argument = nil)
+      return '' unless flag
+      flag = flag.to_s
+      set_pandoc_ruby_options(flag, argument)
+      return '' if flag == 'timeout' # pandoc doesn't accept timeouts yet
+      if !argument.nil?
+        "#{format_flag(flag)} #{argument}"
+      else
+        format_flag(flag)
+      end
+    end
+
+    # Formats an option flag in order to be used with the pandoc command line
+    # tool.
+    def format_flag(flag)
+      if flag.length == 1
+        " -#{flag}"
+      else
+        " --#{flag.to_s.tr('_', '-')}"
+      end
+    end
+
+    # Takes an option and optional argument and uses them to set any flags
+    # used by PandocRuby.
+    def set_pandoc_ruby_options(flag, argument = nil)
+      case flag
+      when 't', 'to'
+        self.writer = argument.to_s
+        self.binary_output = true if BINARY_WRITERS.keys.include?(self.writer)
+      when 'timeout'
+        @timeout = argument
+      end
+    end
 
 end
