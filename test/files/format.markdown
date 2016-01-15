25d9e3996e5d9d73000c308872d2d6ab977f9cce
William Melody <hi@williammelody.com>
2016-01-14 21:06:04 -0800

    Move conversion test strings to individual files.
    Generate tests for conversions using files rather than a hash of
    strings.

---
+++ test/files/format.markdown
@@ -0,0 +1,5 @@
+This is a Title
+===============
+
+Some *emphasized text* and [a
+link](http://daringfireball.net/projects/markdown/)
--- test/test_conversions.rb
+++ test/test_conversions.rb
@@ -1,62 +1,37 @@
 require 'helper'
 
+# Generate tests for converting to and from various formats. Use two nested
+# loops to iterate over each source and destination format, using files with
+# names of the following structure: "format.#{format_name}"
 class TestConversions < Test::Unit::TestCase
-
-  def self.formatted_strings
-    h = {}
-    h[:html] =
-      %Q|<h1 id=\"this-is-a-title\">This is a Title</h1>\n<p>Some <em>emphasized text</em> and <a href=\"http://daringfireball.net/projects/markdown/\">a link</a></p>|
-    h[:html5] =
-      %Q|<h1 id=\"this-is-a-title\">This is a Title</h1>\n<p>Some <em>emphasized text</em> and <a href=\"http://daringfireball.net/projects/markdown/\">a link</a></p>|
-    h[:s5] =
-      %Q|<div id=\"this-is-a-title\" class=\"slide section level1\">\n<h1>This is a Title</h1>\n<p>Some <em>emphasized text</em> and <a href=\"http://daringfireball.net/projects/markdown/\">a link</a></p>\n</div>|
-    h[:slidy] =
-      %Q|<div id=\"this-is-a-title\" class=\"slide section level1\">\n<h1>This is a Title</h1>\n<p>Some <em>emphasized text</em> and <a href=\"http://daringfireball.net/projects/markdown/\">a link</a></p>\n</div>|
-    h[:dzslides] =
-      %Q|<section id=\"this-is-a-title\" class=\"slide level1\">\n<h1>This is a Title</h1>\n<p>Some <em>emphasized text</em> and <a href=\"http://daringfireball.net/projects/markdown/\">a link</a></p>\n</section>|
-    h[:docbook] =
-      %Q|<sect1 id=\"this-is-a-title\">\n  <title>This is a Title</title>\n  <para>\n    Some <emphasis>emphasized text</emphasis> and\n    <ulink url=\"http://daringfireball.net/projects/markdown/\">a\n    link</ulink>\n  </para>\n</sect1>|
-    h[:opendocument] =
-      %Q|<text:h text:style-name=\"Heading_20_1\" text:outline-level=\"1\">This is a\nTitle</text:h>\n<text:p text:style-name=\"First_20_paragraph\">Some\n<text:span text:style-name=\"T1\">emphasized</text:span><text:span text:style-name=\"T2\">\n</text:span><text:span text:style-name=\"T3\">text</text:span> and\n<text:a xlink:type=\"simple\" xlink:href=\"http://daringfireball.net/projects/markdown/\" office:name=\"\"><text:span text:style-name=\"Definition\">a\nlink</text:span></text:a></text:p>|
-    h[:latex] =
-      %Q|\\section{This is a Title}\\label{this-is-a-title}\n\nSome \\emph{emphasized text} and\n\\href{http://daringfireball.net/projects/markdown/}{a link}|
-    h[:beamer] =
-      %Q|\\begin{frame}{This is a Title}\n\nSome \\emph{emphasized text} and\n\\href{http://daringfireball.net/projects/markdown/}{a link}\n\n\\end{frame}|
-    h[:context] =
-      %Q|\\section[this-is-a-title]{This is a Title}\n\nSome {\\em emphasized text} and\n\\useURL[url1][http://daringfireball.net/projects/markdown/][][a\nlink]\\from[url1]|
-    h[:texinfo] =
-      %Q|@node Top\n@top Top\n\n@menu\n* This is a Title::\n@end menu\n\n@node This is a Title\n@chapter This is a Title\n@anchor{#this-is-a-title}\nSome @emph{emphasized text} and\n@uref{http://daringfireball.net/projects/markdown/,a link}|
-    h[:man] =
-      %Q|.SH This is a Title\n.PP\nSome \\f[I]emphasized text\\f[] and a\nlink (http://daringfireball.net/projects/markdown/)|
-    h[:markdown] =
-      %Q|This is a Title\n===============\n\nSome *emphasized text* and [a\nlink](http://daringfireball.net/projects/markdown/)|
-    h[:plain] =
-      %Q|THIS IS A TITLE\n\n\nSome _emphasized text_ and a link|
-    h[:rst] =
-      %Q|This is a Title\n===============\n\nSome *emphasized text* and `a\nlink <http://daringfireball.net/projects/markdown/>`__|
-    h[:mediawiki] =
-      %Q|= This is a Title =\n\nSome ''emphasized text'' and [http://daringfireball.net/projects/markdown/ a link]|
-    h[:textile] =
-      %Q|h1(#this-is-a-title). This is a Title\n\nSome _emphasized text_ and \"a link\":http://daringfireball.net/projects/markdown/|
-    h[:rtf] =
-      %Q|{\\pard \\ql \\f0 \\sa180 \\li0 \\fi0 \\b \\fs36 This is a Title\\par}\n{\\pard \\ql \\f0 \\sa180 \\li0 \\fi0 Some {\\i emphasized text} and {\\field{\\*\\fldinst{HYPERLINK \"http://daringfireball.net/projects/markdown/\"}}{\\fldrslt{\\ul\na link\n}}}\n\\par}|
-    h[:org] =
-      %Q|* This is a Title\n\nSome /emphasized text/ and\n[[http://daringfireball.net/projects/markdown/][a link]]|
-    h[:asciidoc] =
-      %Q|[[this-is-a-title]]\nThis is a Title\n---------------\n\nSome _emphasized text_ and\nhttp://daringfireball.net/projects/markdown/[a link]|
-    return h
+  @extensions = []
+  @file_paths = []
+  @file_paths = Dir.glob(
+    File.join(File.dirname(__FILE__), 'files', 'format*')
+  )
+  @file_paths.each do |f|
+    @extensions << f.match(/format\.(\w+)\Z/)[1]
   end
 
   [:markdown, :html, :rst, :latex].each do |from|
-    formatted_strings.each_key do |format|
-      next if from == format
-      should "convert #{from} to #{format}" do
+    @extensions.each do |to|
+      next if from == to
+      should "convert #{from} to #{to}" do
+        @from_content = File.read(
+          File.join(File.dirname(__FILE__), 'files', "format.#{from}")
+        )
+        @to_content = File.read(
+          File.join(File.dirname(__FILE__), 'files', "format.#{to}")
+        )
         assert_equal(
-          PandocRuby.convert(TestConversions.formatted_strings[from], :from => from, :to => format).strip,
-          TestConversions.formatted_strings[format]
+          PandocRuby.convert(
+            @from_content,
+            :from => from,
+            :to => to
+          ).strip,
+          @to_content.strip
         )
       end
     end
   end
-
 end
