d7f3497f7f7ba0f103e789364ba57bb363221d5d
William Melody <hi@williammelody.com>
2020-02-29 10:58:16 -0800

    Replace 'mocha/setup' require with 'mocha/minitest'.

--- test/helper.rb
+++ test/helper.rb
@@ -9,7 +9,7 @@ rescue Bundler::BundlerError => e
 end
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'mocha/setup'
+require 'mocha/minitest'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
